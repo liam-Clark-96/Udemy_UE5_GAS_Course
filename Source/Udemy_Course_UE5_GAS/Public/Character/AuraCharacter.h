e036f531e7e99dcb4b054eb10984273a60fb5c1c
liam-clark-96 <clarkster996667@hotmail.co.uk>
2025-11-02 10:13:35 +0000

    Player And Enemy Class

---
+++ Source/Udemy_Course_UE5_GAS/Public/Character/AuraCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/AuraCharcaterBase.h"
+#include "AuraCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UDEMY_COURSE_UE5_GAS_API AAuraCharacter : public AAuraCharcaterBase
+{
+	GENERATED_BODY()
+	
+};
