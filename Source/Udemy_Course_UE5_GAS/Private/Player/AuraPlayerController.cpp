b33d6a9b1d41b17681888ada489d8aac4c280c58
liam-clark-96 <clarkster996667@hotmail.co.uk>
2025-11-03 11:46:42 +0000

    Movement Input (11)

--- Source/Udemy_Course_UE5_GAS/Private/Player/AuraPlayerController.cpp
+++ Source/Udemy_Course_UE5_GAS/Private/Player/AuraPlayerController.cpp
@@ -3,28 +3,66 @@
 
 #include "Player/AuraPlayerController.h"
 #include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
 #include "GameFramework/PlayerController.h"
 
 void AAuraPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
 
+	//Chekcing if the AuraContext is valid
 	check(AuraContext);
 	
+	// Get the Enhanced Input Local Player Subsystem to add the mapping context checking it first then adding the context
 	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
 	check(Subsystem);
 	Subsystem->AddMappingContext(AuraContext, 0);
 
+	// Show the mouse cursor and set the default cursor type
 	bShowMouseCursor = true;
 	DefaultMouseCursor = EMouseCursor::Default;
 
+	// Setting the input mode to Game and UI so that both game and UI inputs are processed 
 	FInputModeGameAndUI InputModeData;
 	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);;
 	InputModeData.SetHideCursorDuringCapture(false);
 
 }
 
+// Setting up the input component to bind input actions
+void AAuraPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	//Set up the enhanced input component cast checking it first
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+
+	//Binding the Move action to the Move function 
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AAuraPlayerController::Move);
+}
+
 AAuraPlayerController::AAuraPlayerController()
 {
+	// Enable replication for the player controller to support multiplayer functionality
 	bReplicates = true;
+}
+
+void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	// Getting the input axis vector from the input action value
+	const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>();
+	// Getting the control rotation of the player controller
+	const FRotator Rotation = GetControlRotation();
+	// Isolating the yaw rotation to ignore pitch and roll for movement direction
+	const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+	// Getting the forward and right direction vectors based on the yaw rotation
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+	// Adding movement input to the controlled pawn based on the input axis vector and direction vectors
+	if (APawn* ControlledPawn = GetPawn<APawn>())
+	{
+		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
+		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
+	}
+
 }
--- Source/Udemy_Course_UE5_GAS/Public/Player/AuraPlayerController.h
+++ Source/Udemy_Course_UE5_GAS/Public/Player/AuraPlayerController.h
@@ -6,7 +6,10 @@
 #include "GameFramework/PlayerController.h"
 #include "AuraPlayerController.generated.h"
 
+// Forward declarations of classes and structs used in the AuraPlayerController 
 class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
 
 /**
  * 
@@ -18,6 +21,7 @@ class UDEMY_COURSE_UE5_GAS_API AAuraPlayerController : public APlayerController
 
 protected:
 	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
 
 
 public:
@@ -25,8 +29,15 @@ public:
 	
 
 private:
+	// Input Mapping Context for the Aura Player Controller
 	UPROPERTY(EditAnywhere, Category = "Input")
 	TObjectPtr<UInputMappingContext> AuraContext;
 
+	// Input Action for moving the player
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputAction> MoveAction;
+
+	// Function to handle player movement input
+	void Move(const FInputActionValue& InputActionValue);
 	
 };
