6c24298abb9e7eb9dd0b8c50e61aacdef47fe780
liam-clark-96 <clarkster996667@hotmail.co.uk>
2025-11-06 13:05:07 +0000

    AbilitySystemComponent and AttributeSet

---
+++ Source/Udemy_Course_UE5_GAS/Public/AbilitySystem/AuraAbilitySystemComponent.h
@@ -0,0 +1,17 @@
+// Copyright LiamClark
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "AuraAbilitySystemComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UDEMY_COURSE_UE5_GAS_API UAuraAbilitySystemComponent : public UAbilitySystemComponent
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Udemy_Course_UE5_GAS/Public/AbilitySystem/AuraAttributeSet.h
@@ -0,0 +1,17 @@
+// Copyright LiamClark
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "AuraAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UDEMY_COURSE_UE5_GAS_API UAuraAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};
