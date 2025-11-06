cf17c8b67a16a60e670c83646a1e8150574bbdc2
liam-clark-96 <clarkster996667@hotmail.co.uk>
2025-11-06 15:46:10 +0000

    Replication mode (22)

--- Source/Udemy_Course_UE5_GAS/Private/Player/AuraPlayerState.cpp
+++ Source/Udemy_Course_UE5_GAS/Private/Player/AuraPlayerState.cpp
@@ -7,15 +7,22 @@
 
 AAuraPlayerState::AAuraPlayerState()
 {
+	//Create the ability system component and set it to our custom class
 	AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+	//Create the ability system component and set it to replicate
 	AbilitySystemComponent->SetIsReplicated(true);
+	//Set the replication mode to mixed
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);
 
+	//Create the attribute set and set it to our custom class
 	AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");
 
 	SetNetUpdateFrequency(100.f);
 }
 
+//Implement the GetAbilitySystemComponent function from the IAbilitySystemInterface
 UAbilitySystemComponent* AAuraPlayerState::GetAbilitySystemComponent() const
 {
+	//Return the ability system component
 	return AbilitySystemComponent;
 }
