f0a7729b21a39b3f15c745198a12e40a21886765
liam-clark-96 <clarkster996667@hotmail.co.uk>
2025-11-06 12:52:00 +0000

    The Player State (18)

---
+++ Source/Udemy_Course_UE5_GAS/Public/Player/AuraPlayerState.h
@@ -0,0 +1,20 @@
+// Copyright LiamClark
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "AuraPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UDEMY_COURSE_UE5_GAS_API AAuraPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+
+public:
+	AAuraPlayerState();
+	
+};
